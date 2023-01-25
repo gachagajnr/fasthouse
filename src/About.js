8be9150c88684dc82730996d8abe89b44cfd3542
gachagajnr <williampius17@users.noreply.github.com>
2023-01-25 03:17:33 +0300

    done with fasthouse

--- src/About.js
+++ src/About.js
@@ -1,10 +1,11 @@
 import Container from "react-bootstrap/Container";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
- import Stack from "react-bootstrap/Stack";
- import SocialLinks from "./components/sociallinks";
- 
+import Stack from "react-bootstrap/Stack";
+import { Chat, InfoCircle, EmojiSmile } from "react-bootstrap-icons";
+import SocialLinks from "./components/sociallinks";
+
 export default function Home() {
   return (
     <Container className="vh-100 px-0 align-items-center">
@@ -33,9 +34,8 @@ export default function Home() {
         <div className="me-auto" />
         <SocialLinks className="m-2 text-end" />
       </div>
-     
 
-      <Row className="justify-content-space-around p-5 text-center align-items-center">
+      <Row className="justify-content-space-around p-3 text-center align-items-center">
         <Col sm>
           <div>
             <h2
@@ -44,7 +44,7 @@ export default function Home() {
                 color: "grey",
               }}
             >
-              About
+              About Us
             </h2>
           </div>
         </Col>
@@ -53,13 +53,42 @@ export default function Home() {
         className=" fw-light    "
         style={{ color: "orange", letterSpacing: "3px" }}
       >
-        -YOUR PATA KEJA PARTNER-
+        -PATA KEJA-
+      </p>
+
+      <p
+        className=" fw-light mb-5"
+        style={{ color: "white", letterSpacing: "3px" }}
+      >
+        &bull; Your Partner in Renting, Owning, And Settling &bull;
       </p>
 
       <Stack gap={3}>
-        <div>
-           
-        </div>
+        <Row className="justify-content-center align-items-center">
+          <Col sm>
+            <Chat color="orange" size={90} />
+            <h6 className="mt-3 fw-lighter fs-4" style={{ color: "white" }}>
+              Talk to Us
+            </h6>
+          </Col>
+          &bull;
+          <Col sm>
+            <InfoCircle color="orange" size={100} />
+            <h6 className="mt-3 fw-lighter fs-2" style={{ color: "white" }}>
+              Receive Listings from Our Team
+            </h6>
+          </Col>
+          &bull;
+          <Col sm>
+            <EmojiSmile color="orange" size={90} />
+            <h6 className="mt-3 fw-lighter fs-4" style={{ color: "white" }}>
+              Choose Your Home
+            </h6>
+          </Col>
+        </Row>
+        <h6 className="p-4 fw-light text-primary"  >
+          Your Home Matters
+        </h6>
         <div>
           <h2 style={{ fontWeight: 100, color: "yellow", fontSize: 13 }}>
             {"Copyright © "}{" "}
